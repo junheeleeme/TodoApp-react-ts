1cf714200bd2e95a07b6db2defd32ace5b9e8869
ju_ni <38034518+junheeleeme@users.noreply.github.com>
2021-06-18 03:25:44 +0900

    todo 완료,삭제,수정기능 추가

--- src/components/Todo_Add.jsx
+++ src/components/Todo_Add.jsx
@@ -1,30 +1,29 @@
 const React = require("react");
 const {useState, useRef} = React;
 
-const Todo_Insert = ({AddTodo}) =>{
+const Todo_Add = ({todo_add}) =>{
     const [Todo, setTodo] = useState('insert');
     const input_txt = useRef();
 
     onchange_input = (e) => {
         setTodo(e.target.value);
-    }
+    } 
 
     onSubmit_todo = (e) =>{
         e.preventDefault();
         const val = input_txt.current.value;
 
         if( !(val === '') ){
-            AddTodo(input_txt.current.value);
+            todo_add(input_txt.current.value);
             input_txt.current.value = '';
             input_txt.current.focus();
         }       
             
     }
 
-
     return(
         <React.Fragment>
-            <form action="" className="todo_insert">
+            <form action="" className="todo_add">
                 <input type="text" className="todo_input" onChange={onchange_input} ref={input_txt}/>
                 <button className="insert_btn" onClick={onSubmit_todo} onSubmit={onSubmit_todo}>+</button>
             </form>
@@ -33,4 +32,4 @@ const Todo_Insert = ({AddTodo}) =>{
 }
 
 
-module.exports = Todo_Insert;
+module.exports = Todo_Add;
